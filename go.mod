a8311a09ea061a5f760107946f7206796dca1b8d
GrahamMcBain <grahammcbain@gmail.com>
2020-07-15 21:08:44 -0700

    working example

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/grahammcbain/scavenge
+
+go 1.13
+
+require (
+	github.com/cosmos/cosmos-sdk v0.38.4
+	github.com/cosmos/sdk-tutorials/scavenge v0.0.0-20200714080823-d3516206f600
+	github.com/gorilla/mux v1.7.4
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/viper v1.7.0
+	github.com/tendermint/go-amino v0.15.1
+	github.com/tendermint/tendermint v0.33.3
+	github.com/tendermint/tm-db v0.5.1
+)
---
+++ x/scavenge/types/MsgCommitSolution.go
@@ -0,0 +1,57 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// MsgCommitSolutions
+// ---------------------------------------------------------------------------------------------------
+
+var _ sdk.Msg = &MsgCommitSolution{}
+
+// MsgCommutSolution - struct for unjailing jailed validator
+type MsgCommitSolution struct {
+	Scavenger             sdk.AccAddress `json:"scavenger" yaml:"scavenger"`                         // address of the scavenger
+	SolutionHash          string         `json:"solutionhash" yaml:"solutionhash"`                   // solutionhash of the scavenge
+	SolutionScavengerHash string         `json:"solutionScavengerHash" yaml:"solutionScavengerHash"` // solution hash of the scavenge
+}
+
+// NewMsgCommitSolution creates a new MsgCommitSolution instance
+func NewMsgCommitSolution(scavenger sdk.AccAddress, solutionHash string, solutionScavengerHash string) MsgCommitSolution {
+	return MsgCommitSolution{
+		Scavenger:             scavenger,
+		SolutionHash:          solutionHash,
+		SolutionScavengerHash: solutionScavengerHash,
+	}
+}
+
+// CommitSolutionConst is CommitSolution Constant
+const CommitSolutionConst = "CommitSolution"
+
+// Route noLint
+func (msg MsgCommitSolution) Route() string { return RouterKey }
+func (msg MsgCommitSolution) Type() string  { return CommitSolutionConst }
+func (msg MsgCommitSolution) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{sdk.AccAddress(msg.Scavenger)}
+}
+
+// GetSignBytes gets the bytes for the message signer to sign on
+func (msg MsgCommitSolution) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+//ValidateBasic validity check for the AnteHandler
+func (msg MsgCommitSolution) ValidateBasic() error {
+	if msg.Scavenger.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "creator can't be empty")
+	}
+	if msg.SolutionHash == "" {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "solution can't be empty")
+	}
+	if msg.SolutionScavengerHash == "" {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "solutionScavengerHash can't be empty")
+	}
+	return nil
+}
